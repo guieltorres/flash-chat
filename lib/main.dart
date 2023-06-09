e8c7a21decf55550a8e1920daccb1a5fbffa38a1
Guilherme Torres <torres.me@outlook.com>
2023-06-09 15:47:55 -0300

    feat: initial files

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flash_chat_v2/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.black54),
+          bodyMedium: TextStyle(color: Colors.black54),
+          bodySmall: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
